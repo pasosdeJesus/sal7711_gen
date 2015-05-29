cded794af4e412fad4185f179fecd503b6db1f71
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-05-29 15:22:57 -0500

    Mejor etiqueta

--- lib/sal7711_gen/concerns/models/categoriaprensa.rb
+++ lib/sal7711_gen/concerns/models/categoriaprensa.rb
@@ -6,6 +6,13 @@ module Sal7711Gen
       module Categoriaprensa
         extend ActiveSupport::Concern
         include Sip::Basica
+
+        included do
+          # Para búsquedas tipo autocompletacion en base de datos campos a observar
+          def self.busca_etiqueta_campos
+            ['codigo', 'nombre']
+          end
+        end
       end
     end
   end
