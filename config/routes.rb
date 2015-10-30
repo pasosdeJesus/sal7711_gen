d896a592667ed558eba267dc75824db0b4d8f08e
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-10-30 11:22:43 -0500

    mejores botones de edicion

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Sal7711Gen::Engine.routes.draw do
   get 'codigo' => 'admin/categoriaprensa#busca', as: 'categoriaprensa_busca'
   get 'articulos' => 'buscar#index', as: 'articulos'
   get 'bitacorausuario' => 'bitacora#usuario', as: 'bitacora_usuario'
+
+  get "articulos/nuevo" => 'articulos#new'
   get "articulos/:id" => 'buscar#redirigeindex'
 
   resources :articulos, path_names: { new: 'nuevo', edit: 'edita' },
