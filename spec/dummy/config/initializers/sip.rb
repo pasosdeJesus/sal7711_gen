b6d26ff87ee104acc54f526648993b6f93c35745
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-08-02 07:13:42 -0500

    En ficha, campos de geografia configurables

--- spec/dummy/config/initializers/sip.rb
+++ spec/dummy/config/initializers/sip.rb
@@ -1,8 +1,8 @@
 require 'sal7711_gen/version'
 
 Sip.setup do |config|
-      config.ruta_anexos = Rails.root.join('archivo')
-      config.ruta_volcados = Rails.root.join('archivo', 'bd')
+      config.ruta_anexos = Rails.root.join('archivos')
+      config.ruta_volcados = Rails.root.join('archivos', 'bd')
       # En heroku los anexos son super-temporales
       if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
         config.ruta_anexos = "#{Rails.root}/tmp/"
