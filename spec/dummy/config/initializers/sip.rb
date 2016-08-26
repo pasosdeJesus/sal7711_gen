e93214854085c27a461fc89bc354072e66780a3b
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-08-26 18:30:36 -0500

    nueva organizacion del colchon con directorios por fecha como anexos

--- spec/dummy/config/initializers/sip.rb
+++ spec/dummy/config/initializers/sip.rb
@@ -1,7 +1,8 @@
 require 'sal7711_gen/version'
 
 Sip.setup do |config|
-      config.ruta_anexos = Rails.root.join('archivos')
+      #config.ruta_anexos = Rails.root.join('archivos')
+      config.ruta_anexos = '/var/www/resremoto/archivoprensa/'
       config.ruta_volcados = Rails.root.join('archivos', 'bd')
       # En heroku los anexos son super-temporales
       if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
