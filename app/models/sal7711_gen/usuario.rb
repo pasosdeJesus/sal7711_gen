efaf9f43b5131da84015be0c1d85f7ba499ba4e9
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-09-13 08:24:46 -0500

    usuario sobre Sip::Modelo

--- app/models/sal7711_gen/usuario.rb
+++ app/models/sal7711_gen/usuario.rb
@@ -1,7 +1,11 @@
 # encoding: UTF-8
 
+require 'sip/concerns/models/usuario' 
+
 module Sal7711Gen
-	class Usuario < Sip::Usuario
+	class Usuario < ActiveRecord::Base
+
+    include Sip::Concerns::Models::Usuario  
   
     has_many :bitacoras, foreign_key: 'usuario_id',
       dependent: :destroy, class_name: 'Sal7711Gen::Bitacora'
