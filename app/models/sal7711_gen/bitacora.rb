2191e769f74cc345a5cc0612ad35cd47376a655b
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-04-11 17:19:46 -0500

    trunca antes de agregar a bitacora

--- app/models/sal7711_gen/bitacora.rb
+++ app/models/sal7711_gen/bitacora.rb
@@ -13,6 +13,10 @@ module Sal7711Gen
 
     def self.a(ip, usuario, operacion, detalle, detalle2 = nil, 
                detalle3 = nil)
+      operacion = operacion[0..49] if operacion.length > 50
+      detalle = detalle[0..4999] if detalle.length > 5000
+      detalle2 = detalle2[0..499] if detalle2.length > 500
+      detalle3 = detalle3[0..499] if detalle3.length > 500
       b = Bitacora.new(fecha: Time.now.utc.iso8601,
                    ip: ip,
                    usuario_id: usuario.id,
