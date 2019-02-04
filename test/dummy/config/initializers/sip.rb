c957e29a5394a30e8463e87e09ac10f1161e4213
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-02-04 12:55:53 -0500

    Actualiza

--- test/dummy/config/initializers/sip.rb
+++ test/dummy/config/initializers/sip.rb
@@ -2,7 +2,7 @@ require 'sal7711_gen/version'
 
 Sip.setup do |config|
       #config.ruta_anexos = Rails.root.join('archivos')
-      config.ruta_anexos = Rails.root.join('archivos')
+      config.ruta_anexos = Rails.root.join('archivos', 'anexos')
       config.ruta_volcados = Rails.root.join('archivos', 'bd')
       # En heroku los anexos son super-temporales
       if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
