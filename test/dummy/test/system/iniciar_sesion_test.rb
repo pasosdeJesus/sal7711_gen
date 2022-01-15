a679640765af662f6a13f57f6942dcee64ef38f5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-15 07:28:22 -0500

    Arreglando pruebas al sistema

--- test/dummy/test/system/iniciar_sesion_test.rb
+++ test/dummy/test/system/iniciar_sesion_test.rb
@@ -3,7 +3,9 @@ require "application_system_test_case"
 class IniciarSesionTest < ApplicationSystemTestCase
 
   test "iniciar sesión" do
-    Sip::CapybaraHelper.iniciar_sesion(self, root_path, 'sip', 'sip')
+    Sip::CapybaraHelper.iniciar_sesion(
+      self, Rails.configuration.relative_url_root, 'sal7711', 'sal7711')
+    
   end
 
 end
