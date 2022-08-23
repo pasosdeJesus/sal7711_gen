84ed21aaccdca4e3ec2a851d723886bcb042036e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-23 03:04:04 -0500

    Detecta posible problema al quitar EXTERIOR

---
+++ db/migrate/20220822132000_quita_mun_sal7711.rb
@@ -0,0 +1,13 @@
+class QuitaMunSal7711 < ActiveRecord::Migration[7.0]
+  def up
+    if Sal7711Gen::Articulo.
+        where(departamento_id: 3).count > 0
+      puts "Hay artículos en EXTERIOR pero no hay pais"
+      exit 1
+    end
+  end
+
+  def down
+    raise IrreversibleMigration
+  end
+end
